8e3990135045bcad9522fd166c9733ee198d5ac2
lyq2 <66107255+lyq2@users.noreply.github.com>
2020-08-01 13:08:49 -0400

    moved firebase initializationt to external file

--- App.js
+++ App.js
@@ -1,26 +1,15 @@
 // App.js
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
-import {Image} from 'react-native';
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import * as firebase from "firebase";
-var firebaseConfig = {
-  apiKey: "AIzaSyDmDITr2c11TBklQ6s05KGGh4NALcNOCdk",
-  authDomain: "reacthooks-firebaselogin.firebaseapp.com",
-  databaseURL: "https://reacthooks-firebaselogin.firebaseio.com",
-  projectId: "reacthooks-firebaselogin",
-  storageBucket: "reacthooks-firebaselogin.appspot.com",
-  messagingSenderId: "403004885385",
-  appId: "1:403004885385:web:9f3ba64118d35e09183958"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
+import firebase from './config/config.js';
 import Login from './components/login';
 import Signup from './components/signup';
 import Dashboard from './components/dashboard';
+
 
 const Stack = createStackNavigator();
 
